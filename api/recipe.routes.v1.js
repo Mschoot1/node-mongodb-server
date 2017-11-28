6c5ca789d3331fced0e1381e0f5af53a729f38fc
Mschoot1 <marnix_vd_schoot@hotmail.com>
2017-11-28 12:38:48 +0100

    recipe get and get/:id added

--- api/recipe.routes.v1.js
+++ api/recipe.routes.v1.js
@@ -7,7 +7,8 @@ var express = require('express');
 const RecipeController = require('../controllers/recipe.controller');
 
 module.exports = (app) => {
-    // app.get('api/v1/about', RecipeController.about);
+    app.get('/api/v1/recipes', RecipeController.get);
+    app.get('/api/v1/recipes/:id', RecipeController.getById);
     app.post('/api/v1/recipes', RecipeController.create);
     app.put('/api/v1/recipes:id', RecipeController.update);
     app.put('/api/v1/recipes/addIngredient/:id', RecipeController.addIngredient);
